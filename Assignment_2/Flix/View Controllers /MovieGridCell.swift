fb341579f751b90e4bd1dad7e2300675068508a8
Raziel BenReuben <rbenreuben01@manhattan.edu>
2022-03-07 01:33:35 -0500

    Added TabBarView

---
+++ Assignment_2/Flix/View Controllers /MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by RazBerry_Pi on 3/6/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ Assignment_2/Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by RazBerry_Pi on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
