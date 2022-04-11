2948d45429b62a51cdc5e6bf27d2c2b70748c605
Raziel BenReuben <rbenreuben01@manhattan.edu>
2022-04-11 02:03:04 -0400

    Assignment 4

--- Assignment_3/twitter_ios_starter-2/Twitter/HomeTableViewController.swift
+++ Assignment_3/twitter_ios_starter-2/Twitter/HomeTableViewController.swift
@@ -17,15 +17,18 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
-        
         MyRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = MyRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     @objc func loadTweets(){
-        
-        
         numberOfTweet = 10
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": numberOfTweet]
@@ -100,13 +103,11 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
-    
-    
-        
-    
-    
     
     
     // MARK: - Table view data source
