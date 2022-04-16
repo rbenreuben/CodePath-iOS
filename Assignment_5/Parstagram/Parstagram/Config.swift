41a9ee8811a34e2d67a15a2456ce1e24ee39e7c7
Raziel BenReuben <rbenreuben01@manhattan.edu>
2022-04-16 14:08:16 -0400

    PG completed

---
+++ Assignment_5/Parstagram/Parstagram/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  Parstagram
+//
+//  Created by RazBerry_Pi on 4/11/22.
+//
+
+import Foundation
+
+
+let clientKey = "c03UkO5haDXZD81n8s64mUF5v0WDmeQ6lxDiF1eN"
+let applicationId = "ODwTIDoxOssRJ4tIU8f6JAHH18ND4YUWCvwfzvPS"
