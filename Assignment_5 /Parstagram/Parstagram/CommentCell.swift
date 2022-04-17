aec324440048f514476d1607f6d2b181bbcd65e6
Raziel BenReuben <rbenreuben01@manhattan.edu>
2022-04-17 01:32:02 -0400

    Updated Parstagram Features

---
+++ Assignment_5 /Parstagram/Parstagram/CommentCell.swift
@@ -0,0 +1,29 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by RazBerry_Pi on 4/16/22.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
